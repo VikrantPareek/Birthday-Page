264d424b589e806030f91a63e465e45c5699c1a5
Vikrant Pareek <vikrantpareek001@gmail.com>
2025-05-27 09:56:40 +0530

    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/main", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
