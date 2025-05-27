412ce8562804e3a2088a7b40628192af07859a41
Vikrant Pareek <vikrantpareek001@gmail.com>
2025-05-27 09:45:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/main", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/wish", (req, res) => {
+    let {username} = req.query;
+  res.render("wish.ejs", {username});
+});
+
+app.listen(8080, () => {
+  console.log("server is started");
+});
